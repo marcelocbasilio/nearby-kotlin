b0d3da844232d6937ae0e921be7475746cf847cd
Marcelo Cordeiro Basílio <marcelocbasilio@gmail.com>
2024-12-15 13:21:39 -0300

    Uploading default resources to application settings.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.marcelocbasilio.nearby"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.marcelocbasilio.nearby"
